b8b468eb7f523a47e5edf4ba7aa24f7747e9ad33
zhsrl <thunderccloud@gmail.com>
2021-11-02 16:17:47 +0600

    first task solved

--- task1.dart
+++ task1.dart
@@ -1,3 +1,17 @@
 void main() {
-  print(1);
+  gcd(30, 12);
+}
+
+void gcd(int a, int b) {
+  int? max;
+
+  for (int i = 1; i <= 100; i++) {
+    if (a % i == 0 && b % i == 0) {
+      max = i;
+    } else {
+      continue;
+    }
+  }
+
+  print(max);
 }
